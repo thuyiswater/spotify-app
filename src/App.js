6ba9d2c43fa265988c948dc63afae39ba0d3c414
thuyiswater <s3877746@rmit.edu.vn>
2023-04-13 23:32:05 +0700

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
+import getCurrentUserProfile from "./component/Fetch"
+
+// import FetchData from './component/Fetch';
+
 
 function App() {
+
+  const CLIENT_ID = "dec7d1ad13e3471482f63080e4c6032a"
+  const REDIRECT_URI = "http://localhost:3000/"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+
+
+
+  const [token, setToken] = useState("")
+
+  // const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if (!token && hash) {
+        token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+        window.location.hash = ""
+        window.localStorage.setItem("token", token)
+    }
+    setToken(token)
+}, [])
+
+  const logout = () => {
+    setToken("")
+    window.localStorage.removeItem("token")
+  }
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    setToken(token);
+
+    const fetchData = async () => {
+      try {
+        const { data } = await getCurrentUserProfile();
+        setProfile(data);
+      } catch(e) {
+        console.error(e);
+      }
+    };
+
+    fetchData();
+  }, []);
+  
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            <h1>Spotify API</h1>
+            {!token ?
+                <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
+                    to Spotify</a>
+                : <button onClick={logout}>Logout</button>}
+
+            {profile && (
+                          <div>
+                            <h1>{profile.display_name}</h1>
+                            <p>{profile.followers.total} Followers</p>
+                            {profile.images.length && profile.images[0].url && (
+                              <img src={profile.images[0].url} alt="Avatar"/>
+                            )}
+                          </div>
+                        )}
+        </header>
     </div>
+
   );
 }
 
---
+++ src/component/Fetch.js
@@ -0,0 +1,36 @@
+
+import axios from 'axios'
+
+const getCurrentUserProfile = () => axios.get('/me');
+
+export default getCurrentUserProfile;
+// const FetchData = async (props) => {
+
+//     const [user, setUser] = useState(null)
+//     const {data} = await axios.get('/me', {
+//       headers: {
+//         Authorization: `Bearer ${props.token}`
+//       },
+//       // params: {
+//       //     q: searchKey,
+//       //     type: "artist"
+//       // }
+//     })
+//     setUser(data)
+
+//     return(
+//         <div>
+//         {/* {user && ( */}
+//             <div>
+//                 <h1>{user.display_name}</h1>
+//                 <p>{user.followers.total} Followers</p>
+//                 {user.images.length && user.images[0].url && (
+//                 <img src={user.images[0].url} alt="Avatar"/>
+//                 )}
+//             </div>
+//             {/* )} */}
+//         </div>
+//     );
+//   }
+
+//   export default FetchData;
