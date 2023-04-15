481ce049dbe5cc5729070283145fb4579448a3f7
thuyiswater <s3877746@rmit.edu.vn>
2023-04-15 11:57:35 +0700

    display api

--- src/App.js
+++ src/App.js
@@ -13,11 +13,8 @@ function App() {
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
 
-
-
   const [token, setToken] = useState("")
-
-  // const [user, setUser] = useState(null)
+  const [data, setData] = useState({})
 
   useEffect(() => {
     const hash = window.location.hash
@@ -38,33 +35,69 @@ function App() {
   }
   const [profile, setProfile] = useState(null);
 
+  // useEffect(() => {
+  //   setToken(token);
+
+  //   const fetchData = async () => {
+  //     try {
+  //       const { data } = await getCurrentUserProfile();
+  //       setProfile(data);
+  //     } catch(e) {
+  //       console.error(e);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+  
+  //get track
+  let config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: 'https://api.spotify.com/v1/track?ids=11dFghVXANMlKmJXsNCbNl',
+    headers: { 
+      'Authorization': 'Bearer BQB2u5ZDp4sMW7yHPuFSyj3VzfMMt-zHWBdI_3XcitnM5WpWHw37w-UmHKFAeTzLijWa55l5Q5BRnsO34znOSqUAtCbxN9kOvoEalA9NaaW8ZulkrejIc3NQfg5Jq3a2HEy7YUW4PkpVgH1FbFpsJHBAwHdPtALY_U5TPJvSajHxUkDMtai4fvAFLiIQY5Kk'
+    }
+  };
+  const [dataTemp, setDataTemp] =  useState("");
+
   useEffect(() => {
-    setToken(token);
-
-    const fetchData = async () => {
-      try {
-        const { data } = await getCurrentUserProfile();
-        setProfile(data);
-      } catch(e) {
-        console.error(e);
-      }
-    };
-
-    fetchData();
-  }, []);
+    axios.request(config)
+    .then((response) => {
+      setDataTemp( response.data)
+      console.log(JSON.stringify(response.data))
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   
-  
+  }, [])
 
   return (
     <div className="App">
         <header className="App-header">
             <h1>Spotify API</h1>
             {!token ?
-                <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
+            <div>
+              <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
                     to Spotify</a>
-                : <button onClick={logout}>Logout</button>}
 
-            {profile && (
+            </div>
+                
+                : 
+                <div>
+                  <button onClick={logout}>Logout</button>
+                  <p>{token}</p>
+                  <pre style={{
+                        fontSize: "14px",
+                        textAlign: "left",
+                        padding: "0 50px",
+                        marginLeft: "200px"
+                  }}>{ dataTemp && JSON.stringify(dataTemp, null, "\t") }</pre>
+                </div>
+}
+
+            {/* {profile && (
                           <div>
                             <h1>{profile.display_name}</h1>
                             <p>{profile.followers.total} Followers</p>
@@ -72,7 +105,7 @@ function App() {
                               <img src={profile.images[0].url} alt="Avatar"/>
                             )}
                           </div>
-                        )}
+                        )} */}
         </header>
     </div>
 
